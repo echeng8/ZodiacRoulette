a20451f149686c0d50067a4e381d144bb018ae71
Evan Cheng <dr.frolicks@gmail.com>
2019-05-15 13:31:04 -0700

    u can now grab the balls

---
+++ ZodiacRoulette/Assets/Scripts/Grabable.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// allows the GameObject to be grabbed by mouse and touch
+/// last edited: Evan Cheng
+/// </summary>
+public class Grabable : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnMouseDrag()
+    {
+        float distance_to_screen = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+        transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance_to_screen));
+    }
+}
