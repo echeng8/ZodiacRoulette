dbe6eb76b4b0cd8e381af135422e4e3ca0ee04f8
Santiago Morales <31462534+Aleroms@users.noreply.github.com>
2019-05-24 21:20:27 -0700

    UI complete for prototype

---
+++ ZodiacRoulette/Assets/Scripts/UI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UI : MonoBehaviour
+{
+	private string m_text;
+
+    public void Quit()
+	{
+		print("Quit!\n");
+		Application.Quit();
+	}
+	public void UserInput(string text)
+	{
+		m_text = text;		print(m_text);
+	}
+}
