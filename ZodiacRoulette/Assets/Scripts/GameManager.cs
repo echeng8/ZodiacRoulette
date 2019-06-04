954552ab0e1e75867a6af00f4f79ded579c32241
Santiago Morales <31462534+Aleroms@users.noreply.github.com>
2019-06-03 22:24:54 -0700

    continued on switch mentioned before.

--- ZodiacRoulette/Assets/Scripts/GameManager.cs
+++ ZodiacRoulette/Assets/Scripts/GameManager.cs
@@ -1,7 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events; 
+using UnityEngine.Events;
+using UnityEngine.UI;
 
 public enum Sign {Aquarius, Pisces, Aries, Taurus, Gemini, Cancer, Leo, Virgo, Libra, Scorpio, Sagittarius, Capricorn}
 
@@ -12,9 +13,22 @@ public class GameManager : Singleton<GameManager>
     /// </summary>
     public int signActivationSeconds; 
     public bool signActivated = false;
-    public UnityEvent gameReset;  
+    public UnityEvent gameReset;
 
-    private new void Awake()
+	public Canvas Aqua;
+	public Canvas Pisc;
+	public Canvas Arie;
+	public Canvas Taur;
+	public Canvas Gemi;
+	public Canvas Canc;
+	public Canvas Leo_;
+	public Canvas Virg;
+	public Canvas Libr;
+	public Canvas Scor;
+	public Canvas Sagi;
+	public Canvas Capr;
+
+	private new void Awake()
     {
         base.Awake();  
     }
@@ -31,60 +45,85 @@ public class GameManager : Singleton<GameManager>
         Debug.Log("LANDED ON: " + s);
         signActivated = true;
 
-		switch (s)
+		switch (s) //I feel like this could be done in an
+				//array but idk how arrays work in C#
 		{
 			case Sign.Aquarius:
 				{
+					if(Aqua!= null)
+						Aqua.enabled = true;
 					break;
 				}
 			case Sign.Aries:
 				{
+					if(Arie!= null)
+						Arie.enabled = true;
 					break;
 				}
 			case Sign.Cancer:
 				{
+					if (Canc != null)
+						Canc.enabled = true;
 					break;
 				}
 			case Sign.Capricorn:
 				{
+					if (Capr != null)
+						Capr.enabled = true;
 					break;
 				}
 			case Sign.Gemini:
 				{
+					if (Gemi != null)
+						Gemi.enabled = true;
 					break;
 				}
 			case Sign.Leo:
 				{
+					if (Leo_ != null)
+						Leo_.enabled = true;
 					break;
 				}
 			case Sign.Libra:
 				{
+					if (Libr != null)
+						Libr.enabled = true;
 					break;
 				}
 			case Sign.Pisces:
 				{
+					if (Pisc != null)
+						Pisc.enabled = true;
 					break;
 				}
 			case Sign.Sagittarius:
 				{
+					if (Sagi != null)
+						Sagi.enabled = true;
 					break;
 				}
 			case Sign.Scorpio:
 				{
+					if (Scor != null)
+						Scor.enabled = true;
 					break;
 				}
 			case Sign.Taurus:
 				{
+					if (Taur != null)
+						Taur.enabled = true;
 					break;
 				}
 			case Sign.Virgo:
 				{
+					if (Virg != null)
+						Virg.enabled = true;
 					break;
 				}
-			//default:
+				//default:
 				//print("Error GameManager.cs line 85");
 		}
-				
-    } 
+
+	} 
 
 }
