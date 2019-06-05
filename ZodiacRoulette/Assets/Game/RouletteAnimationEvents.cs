819e07db17d9b61e8014e4a4705a00da3fe10769
Ervie Tubig <etubig@uci.edu>
2019-06-05 06:22:01 -0700

    Intro text to throw sequence (start)
    added a seek particle script as well as an animation event transitioning from text to game loop

---
+++ ZodiacRoulette/Assets/Game/RouletteAnimationEvents.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RouletteAnimationEvents : MonoBehaviour
+{
+    public void StartSpin()
+    {
+        Spin spin = GetComponent<Spin>();
+        spin.enabled = true;
+    }
+}
